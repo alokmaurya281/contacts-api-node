0729e0936b39f7021a4defe6cf2a3ccfe79d09d2
Alok Maurya <alok.maurya@w3villa.com>
2023-10-06 15:16:18 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5001;
+const cors = require('cors');
+app.use(cors());
 
 connectDB();
 
@@ -15,15 +17,12 @@ app.use("/api/v1/contacts", require("../routes/contactRoutes"));
 app.use("/api/v1/user", require("../routes/userRoutes"));
 
 app.use(errorHandler);
-var cors = require('cors');
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 app.listen(port, () => {
